077d8eb3781c66425f25bdeb45fd40fb2af03cd5
Adam Froio <afroio@gmail.com>
2021-12-20 17:01:47 -0800

    Chapter 10

--- NyetHack/src/main/kotlin/Tavern.kt
+++ NyetHack/src/main/kotlin/Tavern.kt
@@ -8,7 +8,7 @@ private const val TAVERN_NAME = "$TAVERN_MASTER's Folly"
 private val firstNames = setOf("Alex", "Mordoc", "Sophie", "Tariq")
 private val lastNames = setOf("Ironfoot", "Fernsworth", "Baggins", "Downstrider")
 
-private val menuData = File("NyetHack/data/tavern-menu-data.txt")
+private val menuData = File("data/tavern-menu-data.txt")
     .readText()
     .split("\n")
 
@@ -22,39 +22,147 @@ private val menuPrices = List(menuData.size) { index ->
     price
 }
 
+private val menuTypes = List(menuData.size) { index ->
+    val (type, _, _) = menuData[index].split(",")
+    type
+}
+
+
 
 fun visitTavern () {
     narrate("$heroName enters $TAVERN_NAME")
     narrate("There are several items for sale:")
 
-    //Chapter 8 Challenge 1
-    var longest: Int = menuItems[0].length
-
-    // get longest menu item
-    menuItems.forEachIndexed { index, item ->
-        if (item.length > longest) { longest = item.length }
-    }
-
-    narrate(menuItems.joinToString())
+     narrate(menuItems.joinToString())
 
     val patrons: MutableSet<String> = mutableSetOf()
-    while (patrons.size < 10) {
-        patrons += "${firstNames.random()} ${lastNames.random()}"
+    val patronGold = mutableMapOf(
+        TAVERN_MASTER to 86.0,
+        heroName to 4.50
+    )
+
+    while (patrons.size < 5) {
+        val patronName = "${firstNames.random()} ${lastNames.random()}"
+        patrons += patronName
+        patronGold += patronName to 6.0
     }
 
     narrate("$heroName sees several patrons in the tavern:")
     narrate(patrons.joinToString())
 
-    repeat(patrons.size) {
-        placeOrder(patrons.random(), menuItems.random())
+    repeat(3) {
+        placeOrder(patrons.random(), menuItems.random(), patronGold)
+    }
+    println(patronGold)
+
+
+}
+
+
+private fun placeOrder(
+    patronName: String,
+    menuItemName: String,
+    patronGold: MutableMap<String, Double>
+) {
+    val itemPrice = 1.0
+    narrate("$patronName places an order with $TAVERN_MASTER.")
+    if (itemPrice <= patronGold.getOrDefault(patronName, 0.0)) {
+        narrate("$TAVERN_MASTER hands $patronName a plate of $menuItemName.")
+        narrate("$patronName pays $TAVERN_MASTER $itemPrice gold")
+        patronGold[patronName] = patronGold.getValue(patronName) - itemPrice
+        patronGold[TAVERN_MASTER] = patronGold.getValue(TAVERN_MASTER) + itemPrice
+    } else {
+        narrate("$TAVERN_MASTER says, \"You need more coin for a $menuItemName\"")
+    }
+}
+
+
+
+// Chapter 9 Challenges
+fun printMenu() {
+
+    //Chapter 9 Challenges
+    var longestItem = 0
+    var numItems = 0
+    var longestLine = 0
+
+    // get longest menu item
+    menuItems.forEach { item ->
+        if (item.length > longestItem) {
+            longestItem = item.length
+        }
+        numItems++
+    }
+
+    // get longest menu line (item + price)
+    var i = 0
+    while (i < numItems) {
+        if (menuItems[i].length + menuPrices[i].length - 1 > longestLine) {
+            longestLine = menuItems[i].length + menuPrices[i].length
+        }
+        i++
+    }
+
+    println("\r\n*** Welcome to $TAVERN_NAME ***")
+    i = 0
+    var menuType = "shandy"
+    var typePadding = (longestLine - menuType.length) / 2
+    println(" ".repeat(typePadding) + "~[$menuType]~")
+    while (i < numItems) {
+        if (menuTypes[i].lowercase() == menuType) {
+            var lineLength = menuItems[i].length + menuPrices[i].trim().length
+            var lineDiff = longestLine - lineLength
+            println(menuItems[i] + ".".repeat(lineDiff + 5) + menuPrices[i])
+        }
+        i++
+    }
+    i = 0
+    menuType = "elixir"
+    typePadding = (longestLine - menuType.length) / 2
+    println(" ".repeat(typePadding) + "~[$menuType]~")
+    while (i < numItems) {
+        if (menuTypes[i].lowercase() == menuType) {
+            var lineLength = menuItems[i].length + menuPrices[i].trim().length
+            var lineDiff = longestLine - lineLength
+            println(menuItems[i] + ".".repeat(lineDiff + 5) + menuPrices[i])
+        }
+        i++
+    }
+    i = 0
+    menuType = "meal"
+    typePadding = (longestLine - menuType.length) / 2
+    println(" ".repeat(typePadding) + "~[$menuType]~")
+    while (i < numItems) {
+        if (menuTypes[i].lowercase() == menuType) {
+            var lineLength = menuItems[i].length + menuPrices[i].trim().length
+            var lineDiff = longestLine - lineLength
+            println(menuItems[i] + ".".repeat(lineDiff + 5) + menuPrices[i])
+        }
+        i++
+    }
+    i = 0
+    menuType = "desert dessert"
+    typePadding = (longestLine - menuType.length) / 2
+    println(" ".repeat(typePadding) + "~[$menuType]~")
+    while (i < numItems) {
+        if (menuTypes[i].lowercase() == menuType) {
+            var lineLength = menuItems[i].length + menuPrices[i].trim().length
+            var lineDiff = longestLine - lineLength
+            println(menuItems[i] + ".".repeat(lineDiff + 5) + menuPrices[i])
+        }
+        i++
+    }
+    i = 0
+    menuType = "deserved dessert"
+    typePadding = (longestLine - menuType.length) / 2
+    println(" ".repeat(typePadding) + "~[$menuType]~")
+    while (i < numItems - 1) {
+        if (menuTypes[i].lowercase() == menuType) {
+            var lineLength = menuItems[i].length + menuPrices[i].trim().length
+            var lineDiff = longestLine - lineLength
+            println(menuItems[i] + ".".repeat(lineDiff + 5) + menuPrices[i])
+        }
+        i++
     }
 
 }
-
-
-private fun placeOrder(patronName: String, menuItemName: String) {
-    narrate("$patronName places an order with $TAVERN_MASTER.")
-    narrate("$TAVERN_MASTER hands $patronName a plate of $menuItemName.")
-}
-
-
